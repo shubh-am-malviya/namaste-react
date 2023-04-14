0c05e9f82a9ace144e5df0b8d912ed40522bb263
Shubham Malviya <edu.shubham01@gmail.com>
2023-04-14 22:12:40 +0530

    Parcel installation and setup

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
-// const heading = React.createElement("h1", {}, "Hello World from react");
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div", { id: "parent" }, [
 	React.createElement("div", { id: "child1" }, [
