5c6ff5a251f8513bad3bbd91032e8b4a38755e82
Shubham Malviya <edu.shubham01@gmail.com>
2023-06-29 23:16:31 +0530

    E08: Let's get classy

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,37 @@
 import React from "react";
+import User from "./User";
+import UserClass from "./UserClass";
 
-const About = () => {
-	return <div>About</div>;
-};
+class About extends React.Component {
+	constructor(props) {
+		super(props);
+		// console.log("Parent Constructor");
+	}
+
+	componentDidMount() {
+		// console.log("Parent componentDidMount");
+	}
+
+	render() {
+		// console.log("Parent Render");
+
+		return (
+			<div>
+				<h1>About Class Component</h1>
+				<h2>This is Namaste React Web Series</h2>
+				<UserClass name={"Akshay Saini Class"} location={"Dehradun"} />
+			</div>
+		);
+	}
+}
+
+// const About = () => {
+// 	return (
+// 		<div>
+// 			<h1>About</h1>
+// 			<UserClass name={"Akshay Saini Class"} location={"Dehradun"} />
+// 		</div>
+// 	);
+// };
 
 export default About;
